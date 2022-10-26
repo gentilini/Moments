9f153c4f8c7ae05bfa6e35b65b2d5b809f1928b5
Rafael Gentilini <gentilini.ads@gmail.com>
2022-10-25 23:17:50 -0300

    home page

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 
+import { Moment } from '../moment';
+import { Response } from '../response';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,6 +15,10 @@ export class MomentService {
   private apiUrl = `${this.baseApiUrl}api/moments`;
 
   constructor(private http: HttpClient) { }
+
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
